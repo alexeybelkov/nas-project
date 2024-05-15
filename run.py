defaaadde4611be8b9f8d96da5befcebd6c21db6
Alexey <alexey.belkov@ira-labs.com>
2024-05-15 22:30:01 +0300

    finale 2

--- run.py
+++ run.py
@@ -16,32 +16,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str)
     parser.add_argument('--device', type=str, default='cuda')
-    # parser.add_argument('seed', type=int, default=0)
-    # parser.add_argument('input_dim', type=int, default=5)
-    # parser.add_argument('num_workers', type=int, default=8)
-    # parser.add_argument('batch_size', type=int, default=512)
-    # parser.add_argument('num_threads', type=int, default=1)
-    # parser.add_argument('hidden_dim', type=int, default=20)
-    # parser.add_argument('lr', type=float, default=1e-2)
-    # parser.add_argument('num_epochs', type=int, default=20)
-    # parser.add_argument('num_launches', type=int, default=4)
-    # parser.add_argument('num_epochs', type=int, default=1e-20)
 
     args = parser.parse_args()
     config = deli.load_json(args.config)
 
     config['device'] = args.device
-    # SEED = config["seed"]
-    # INPUT_DIM = config["input_dim"]
-    # NUM_WORKERS = config["num_workers"]
-    # BATCH_SIZE = config["batch_size"]
-    # NUM_THREADS = config["num_threads"]
-    # HIDDEN_DIM = config["hidden_dim"]
-    # LR = config["lr"]
-    # NUM_EPOCHS = config["num_epochs"]
-    # NUM_LAUNCHES = config["num_launches"]
-    # GAIN = config["gain"]
-    # SPACE_SIZE = config["space_size"]
+
 
 
     from src.train import (train_classic_mlp, 
